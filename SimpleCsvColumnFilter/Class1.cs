5052a90329c8c6d8165140ebcaf607735016c22a
Josef Wurzel <jaykay2k@hotmail.com>
2015-03-27 22:14:16 +0100

    cs part of simple csv prog

---
+++ SimpleCsvColumnFilter/Class1.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace SimpleCsvColumnFilter
+{
+	[TestFixture]
+	public class SimpleCsvColumnFilterTest
+    {
+		[Test]
+		public void Test01()
+		{
+			// Arrange
+			var originalCsvPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "input.csv");
+			var pathToCsvFile = Path.Combine(Path.GetDirectoryName(originalCsvPath), Path.GetFileNameWithoutExtension(originalCsvPath) + "_Test.csv");
+			var delimiter = ";";
+			File.Copy(originalCsvPath, pathToCsvFile);
+			Filter sut = new Filter() { Url = pathToCsvFile, ColumnIndexToIgnore = 2, Delimiter = delimiter };
+
+			// Act
+			sut.Process();
+
+			// Assert
+			var lines = File.ReadAllLines(pathToCsvFile).Select(line => line.Split(new string[]{delimiter}, StringSplitOptions.None));
+
+			lines.Should().OnlyContain(splittedLine => splittedLine.Length == 3);
+			lines.ElementAt(0).ElementAt(0).Should().Be("A");
+			lines.ElementAt(0).ElementAt(0).Should().Be("B");
+			lines.ElementAt(0).ElementAt(0).Should().Be("D");
+			lines.ElementAt(1).ElementAt(0).Should().Be("1");
+			lines.ElementAt(1).ElementAt(0).Should().Be("2");
+			lines.ElementAt(1).ElementAt(0).Should().Be("4");
+
+		}
+    }
+}
---
+++ SimpleCsvColumnFilter/Filter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Linq;
+
+namespace SimpleCsvColumnFilter
+{
+	public class Filter
+	{
+		public string Url { get; set; }
+
+		public int ColumnIndexToIgnore { get; set; }
+		
+		public void Process()
+		{
+			var splitLines = File.ReadAllLines(Url).Select(line => line.Split(new []{Delimiter}, StringSplitOptions.None));
+
+			var filteredLines = splitLines.Select(splitLine => splitLine.Take(ColumnIndexToIgnore).Concat(splitLine.Skip(ColumnIndexToIgnore))).Select(line => string.Join(Delimiter, line.ToArray()));
+
+			File.Delete(Url);
+			File.WriteAllLines(Url, filteredLines);
+		}
+
+		public string Delimiter { get; set; }
+	}
+}
