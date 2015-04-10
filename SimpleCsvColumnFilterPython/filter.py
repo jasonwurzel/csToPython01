47ce1025a5ef99dd979be461206ce94ce2fdf75f
Josef Wurzel <jaykay2k@hotmail.com>
2015-04-10 20:15:12 +0200

    first naive implementation (hey, my first lines in python :-)

--- SimpleCsvColumnFilterPython/filter.py
+++ SimpleCsvColumnFilterPython/filter.py
@@ -1,6 +1,6 @@
 __author__ = 'Milly'
 index = 2
-with open('filename') as myFile:
+with open('C:\Users\Milly\Documents\Visual Studio 2013\Projects\SimpleCsvColumnFilter\Tests\FilterThirdCol\input.csv') as myFile:
     lines = [line.rstrip('\n') for line in myFile]
     filteredLines = []
     for line in lines:
@@ -8,7 +8,7 @@ with open('filename') as myFile:
         filteredWords = []
         for w in words[0:index]:
             filteredWords.append(w)
-        for w in words[index:len(words)]:
+        for w in words[index+1:len(words)]:
             filteredWords.append(w)
         filteredLine = ";".join(filteredWords)
         filteredLines.append(filteredLine)
