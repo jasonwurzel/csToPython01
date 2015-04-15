bd70a8f2496af106ab29ebc3688f5bd1cace1475
Josef Wurzel <jaykay2k@hotmail.com>
2015-04-15 20:52:53 +0200

    tha shizzle

--- SimpleCsvColumnFilterPython/filter.py
+++ SimpleCsvColumnFilterPython/filter.py
@@ -1,6 +1,7 @@
 __author__ = 'Milly'
 
 import os
+import csv
 
 
 class FilterCsvFile():
@@ -8,16 +9,12 @@ class FilterCsvFile():
         pass
 
     @staticmethod
-    def process(fileName, indexToOmit):
-        with open(fileName) as myFile:
-            lines = [line.rstrip('\n') for line in myFile]
-            filteredLines = []
-            for line in lines:
-                words = line.split(';')
-                filteredWords = words[:indexToOmit] + words[indexToOmit + 1:]
-                filteredLine = ";".join(filteredWords)
-                filteredLines.append(filteredLine + "\n")
-        outputFile = os.path.splitext(fileName)[0] + '_filtered' + os.path.splitext(fileName)[1]
-        with open(outputFile, 'w') as myFilteredFile:
-            myFilteredFile.writelines(filteredLines)
-        return outputFile
+    def process(inputfilepath, indexToOmit):
+        outputfilepath = os.path.splitext(inputfilepath)[0] + '_filtered' + os.path.splitext(inputfilepath)[1]
+        with open(inputfilepath, 'rb') as csvfile, open(outputfilepath, 'wb') as myFilteredFile:
+            reader = csv.reader(csvfile, delimiter=';')
+            writer = csv.writer(myFilteredFile, delimiter=';')
+            for line in reader:
+                filteredWords = line[:indexToOmit] + line[indexToOmit + 1:]
+                writer.writerow(filteredWords)
+        return outputfilepath
